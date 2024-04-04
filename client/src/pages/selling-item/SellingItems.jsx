5a1604e98d02848bc21577873e502300cd1a5b38
GurleenA <101472021+GurleenA@users.noreply.github.com>
2024-04-04 01:23:05 -0600

    feat(selling-item): Implemented carousel for multiple images
    Added a carousel feature to the selling item page, allowing users to browse through multiple images for that product

--- client/src/pages/selling-item/SellingItems.jsx
+++ client/src/pages/selling-item/SellingItems.jsx
@@ -2,43 +2,56 @@ import React from "react";
 import { useState } from "react";
 import {Button, Nav, Navbar, NavDropdown, Offcanvas, Row} from "react-bootstrap";
 import {Col, Container, Card, Badge, Image} from "react-bootstrap";
-import {Link, useNavigate} from "react-router-dom";
+import {Link, useLocation, useNavigate} from "react-router-dom";
 import "./SellingItems.scss";
 import locationLogo from "../../assets/selling-items/location.svg";
 import condition from "../../assets/selling-items/condition.svg";
 import rating from "../../assets/selling-items/rating.svg";
 import email from "../../assets/selling-items/email.svg";
 import number from "../../assets/selling-items/phoneNumber.svg";
+import Carousel from "react-bootstrap/Carousel";
 
-const SellingItem = () => {
+
+const SellingItem = (props) => {
+    const {state} = useLocation();
     return(
         <Container className=" h-100 w-100">
             <Row md={2} xs={1} >
                 <Col className="col-length">
-                    <div className="contain-image">
-                        <Image
-                            src="https://crystal-cdn4.crystalcommerce.com/photos/6772128/430px-GyaradosPok%C3%A9monGO22.jpg"
-                            className="post-img"
-                            fluid
-                        />
+                    <div className="contain-image" id="selling-carousel">
+                        {state.image.length > 1 ? (
+                                <Carousel className="selling-item"  slide={false}>
+                                    {state.image.map((image, index) => (
+                                        <Carousel.Item key={index}>
+                                            <Card.Img
+                                                className="post-img"
+                                                src={image}
+                                                alt={`Slide ${index}`}
+                                            />
+                                        </Carousel.Item>
+                                    ))}
+                                </Carousel>
+                            ) : //acts as an else
+                            <Card.Img variant="top" className="post-img" src={state.image[0]} />
+                        }
                     </div>
                 </Col>
                 <Col className="col-info-color">
                     <div className=" information-hold">
 
                     <div className="d-flex justify-content-between">
-                        <h4>Gyarados</h4>
-                        <Badge bg="HHPurple" className="badge d-flex justify-content-center align-items-center">Pokemon</Badge>
+                        <h4>{state.title}</h4>
+                        <Badge bg="HHPurple" className="badge d-flex justify-content-center align-items-center">{state.tag}</Badge>
                     </div>
-                    <p className="info-text">$3.50</p>
+                    <p className="info-text">${state.price}</p>
                     <div className="d-flex justify-content-between p-0 mb-0">
                         <div className="d-flex">
                             <Image src={locationLogo} className="logo"/>
-                            <p className="info-text">South Calgary</p>
+                            <p className="info-text">{state.location}</p>
                         </div>
                         <div className="d-flex">
                             <Image src={condition} className="logo"/>
-                            <p className="info-text">Open Box</p>
+                            <p className="info-text">{state.condition}</p>
                         </div>
                         <div className="d-flex">
                             <Image src={rating} className="logo"/>
@@ -47,17 +60,15 @@ const SellingItem = () => {
                     </div>
                     <div className="d-flex">
                         <Image src={email} className="logo"/>
-                        <p className="info-text">example@gmail.com</p>
+                        <p className="info-text">{state.email}</p>
                     </div>
                     <div className="d-flex">
                         <Image src={number} className="logo"/>
-                        <p className="info-text">541 254 5878</p>
+                        <p className="info-text">{state.phone}</p>
                     </div>
 
                     <h5>Description</h5>
-                    <p className="info-text">Good Condition Gyarados card email me for more information.
-                    I am open to trades as well. Located South Calgary.
-                    </p>
+                    <p> {state.description}</p>
                     </div>
                 </Col>
             </Row>
